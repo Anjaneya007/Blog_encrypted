dde69cf72f68689c642dbf5187f070d7c717ad12
Anjaneya007 <125915045+Anjaneya007@users.noreply.github.com>
2023-08-14 22:59:03 +0530

    content update

--- app.js
+++ app.js
@@ -40,9 +40,13 @@ app.use(session({
 
 // mongoose.connect('mongodb://127.0.0.1:27017/blogDB');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Step into a world of inspiration, insights, and imagination. Daily Journal is your canvas for expression, your sanctuary for ideas. Join us in celebrating the beauty of words and the stories they weave. Welcome aboard!";
+const aboutContent = "At Daily Journal, we're passionate about words and the stories they tell. Our platform is a canvas for thought-provoking ideas, creative expressions, and meaningful discussions. Join us in exploring the boundless world of words and sharing in the art of storytelling. Welcome to our community!";
+const contactContent = "Have a question, suggestion, or a story you want to share? We'd love to hear from you! At Daily Journal, we believe in the power of meaningful conversations and building connections./n"+
+
+"Feel free to reach out to us at contact@dailyjournal.com for any inquiries, feedback, or collaboration opportunities. We value your input and look forward to engaging with you./n"+
+
+"Join us in shaping the narrative, one interaction at a time.";
 
 
 
